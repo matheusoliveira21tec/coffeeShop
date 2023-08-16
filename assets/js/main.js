0725a4684611eab23fdc3ac88b2aa50b6a17185e
monegasco <mhoa.2000@gmail.com>
2023-08-15 22:43:35 -0300

    final commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,10 @@
 /*=============== LOADER ===============*/
-
+onload = () => {
+    const load = document.getElementById('load');
+    setTimeout(() => {
+        load.style.display = 'none';
+    },2500);
+}
 
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav-menu'),
@@ -63,6 +68,31 @@ function activeLinkProducts() {
 linkProducts.forEach(l => l.addEventListener('click', activeLinkProducts))
 
 /*=============== SHOW SCROLL UP ===============*/
-
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+    const scrollDown = window.scrollY
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('active-link')
+        } else {
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
